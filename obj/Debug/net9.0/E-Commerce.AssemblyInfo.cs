c26647f30c7a7cc923c1ffda20a0d3e9040548ee
omargamal1121 <omargamal1132004@gmail.com>
2025-08-25 18:31:13 +0300

    last update

--- obj/Debug/net9.0/E-Commerce.AssemblyInfo.cs
+++ obj/Debug/net9.0/E-Commerce.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2babba216febc6d959e5866d6c411f133cd41949")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b82de05e9db0826d601ca37232d932db968b748f")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
